9b9a2bda4193aa9c7d8013a87cb8ff7fc458ef33
john li <jetpack@google.com>
2016-03-10 00:44:12 -0500

    Include EpiDataStore's load promise in MapColoring's load promise.

--- public/model/map-coloring.js
+++ public/model/map-coloring.js
@@ -17,7 +17,8 @@ var MapColoring = P({
       oviposition: init_dict.weather_data_store.fake_oviposition_model()
     };
     this.epi_data_store = init_dict.epi_data_store;
-    this.initial_load_promise = Q.all([init_dict.weather_data_store.initial_load_promise]);
+    this.initial_load_promise = Q.all([init_dict.weather_data_store.initial_load_promise,
+                                       this.epi_data_store.initial_load_promise]);
   },
 
   active_base_layer_data_store: function() {
@@ -51,7 +52,7 @@ var MapColoring = P({
   epi_data_to_severity: function(epi_data) {
     return this.epi_data_store.epi_data_to_severity(epi_data);
   },
-  
+
   epi_data_to_html_string: function(epi_data) {
     return this.epi_data_store.epi_data_to_html_string(epi_data);
   }
