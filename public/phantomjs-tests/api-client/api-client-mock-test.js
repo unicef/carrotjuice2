d72f79e350dcd2e133c7dd0e4b028d4468f66a6f
zora tung <gatoatigrado@gmail.com>
2016-03-12 22:19:24 -0800

    Make it pass lint

--- public/phantomjs-tests/api-client/api-client-mock-test.js
+++ public/phantomjs-tests/api-client/api-client-mock-test.js
@@ -8,21 +8,21 @@ var assert = require('assert');
 var MockApiClient = require('./api-client-mock.js');
 
 describe('api-client/api-client-mock', function() {
-    it('returns topojson', function() {
-        var client = new MockApiClient('br');
-        return client.fetch_region_data().then(function(data) {
-            assert.strictEqual(data.type, "Topology");
-            assert.strictEqual(data.objects.collection.type, "GeometryCollection");
-            assert.strictEqual(
-                data.objects.collection.geometries[0].properties.country_code, "br"
-            );
-        });
+  it('returns topojson', function() {
+    var client = new MockApiClient('br');
+    return client.fetch_region_data().then(function(data) {
+      assert.strictEqual(data.type, "Topology");
+      assert.strictEqual(data.objects.collection.type, "GeometryCollection");
+      assert.strictEqual(
+        data.objects.collection.geometries[0].properties.country_code, "br"
+      );
     });
-    
-    it('returns weather data', function() {
-        var client = new MockApiClient('br');
-        return client.fetch_weather_data().then(function(data) {
-            assert.strictEqual(lodash.values(data)[0][4877].temp_mean, 3.81);
-        })
-    })
+  });
+
+  it('returns weather data', function() {
+    var client = new MockApiClient('br');
+    return client.fetch_weather_data().then(function(data) {
+      assert.strictEqual(lodash.values(data)[0][4877].temp_mean, 3.81);
+    });
+  });
 });
--- public/phantomjs-tests/api-client/api-client-mock.js
+++ public/phantomjs-tests/api-client/api-client-mock.js
@@ -8,24 +8,24 @@ var Q = require('q');
 var mock_topojson_result = require('./mock_topojson_result.json');
 
 var APIClient = P({
-  init: function (country_code) {
+  init: function(country_code) {
     this.country_code = country_code;
   },
   fetch_region_data: function() {
     return Q(mock_topojson_result);
   },
-  fetch_weather_data: function(date) {
+  fetch_weather_data: function() {
     return Q({
       "2016-03-10T00:00:00.000Z": {
-        "4877": {
-          "temp_mean": 3.81
+        4877: {
+          temp_mean: 3.81
         }
       }
     });
   },
-  fetch_region_weather_data: function (region_code, num_days) {
+  fetch_region_weather_data: function(region_code) {
     var inner = {};
-    inner[region_code] = {"temp_mean": 5};
+    inner[region_code] = {temp_mean: 5};
     return Q({
       "2016-03-10T00:00:00.000Z": inner
     });
