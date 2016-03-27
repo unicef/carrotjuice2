8c7bf76c6ebf4e68ae32c2d539b259b3c2fed2c6
john li <jetpack@google.com>
2016-03-27 17:07:53 -0400

    i <3 newlines and snake_case

--- public/api-client/api-client.js
+++ public/api-client/api-client.js
@@ -8,7 +8,7 @@ var Q = require('q');
 var jQuery = require('jquery');
 var DateUtil = require('../model/date-util.js');
 
-var makeRequest = function(url) {
+var make_request = function(url) {
   var deferred = Q.defer();
 
   jQuery.ajax({
@@ -27,8 +27,9 @@ var makeRequest = function(url) {
 
 var APIClient = P({
   fetch_admin_data: function(country_code) {
-    return makeRequest("/api/admin_polygons_topojson/" + country_code);
+    return make_request("/api/admin_polygons_topojson/" + country_code);
   },
+
   fetch_country_weather_data: function(country_code, date) {
     var date_str = '';
     if (date) {
@@ -36,21 +37,23 @@ var APIClient = P({
     } else {
       console.log('No date specified - fetching latest available data..');
     }
-    return makeRequest('/api/country_weather/' + country_code + date_str);
+    return make_request('/api/country_weather/' + country_code + date_str);
   },
+
   fetch_admin_weather_data: function(admin_code, num_days) {
     if (num_days === undefined) {
       num_days = 180;
     }
     var today = new Date();
     var start_date = DateUtil.subtract_days(today, num_days);
-    return makeRequest('/api/admin_weather/' + admin_code + '/' +
+    return make_request('/api/admin_weather/' + admin_code + '/' +
                        DateUtil.iso_to_yyyymmdd(start_date) + '/' +
                        DateUtil.iso_to_yyyymmdd(today));
   },
+
   fetch_egress_mobility_data: function(admin_code, date) {
     var d = '/' + DateUtil.iso_to_yyyymmdd(date);
-    return makeRequest('/api/egress_mobility/' + admin_code + d + d);
+    return make_request('/api/egress_mobility/' + admin_code + d + d);
   }
 });
 
