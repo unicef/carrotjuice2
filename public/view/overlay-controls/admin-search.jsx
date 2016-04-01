1d1a42b9a572ce30f77e069158d336832d0272c5
mikefab <mikefabrikant@gmail.com>
2016-04-01 13:42:55 -0400

    Rename lookup variable.

--- public/view/overlay-controls/admin-search.jsx
+++ public/view/overlay-controls/admin-search.jsx
@@ -3,9 +3,12 @@ var Typeahead = require('react-typeahead').Typeahead;
 require('./admin-search.css');
 
 var AdminSearch = React.createClass({
+
+  search_key_to_admin: {},
+
   admins: function() {
     var admin_objs = this.props.admin_details.admin_data_by_code;
-
+    var search_key_to_admin = this.search_key_to_admin;
     // Maintain array of selected country iso codes
     var available_countries = Object.keys(
       this.props.selected_countries.selected_country_codes
@@ -20,18 +23,16 @@ var AdminSearch = React.createClass({
       });
     // Return array of admin names
     }).map(function(key) {
-      return admin_objs[key].name + ' ' + admin_objs[key].admin_code;
+      var admin_code = admin_objs[key].admin_code;
+      var name_code = admin_objs[key].name + ' ' + admin_code;
+      search_key_to_admin[name_code] = admin_code;
+      return name_code;
     }).sort();
     return admins;
   },
 
   optionSelected: function(admin_name_code) {
-    var admin_objs = this.props.admin_details.admin_data_by_code;
-    var admin_code = Object.keys(this.props.admin_details.admin_data_by_code)
-    .find(function(key) {
-      return admin_objs[key].name + ' ' + admin_objs[key].admin_code === admin_name_code;
-    });
-
+    var admin_code = this.search_key_to_admin[admin_name_code];
     this.props.admin_details.selected_admins.select_admin(admin_code);
     this.props.admin_details.selected_admins.search_admin(admin_code);
   },
